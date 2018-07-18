373597ab94e6097a5cfad56d7a34e5a9232c173a
Anoop Varghese <anupvarghese@gmail.com>
2018-07-18 23:16:25 +1000

    add more

--- src/TodoApp.bs.js
+++ src/TodoApp.bs.js
@@ -3,10 +3,19 @@
 
 var List = require("bs-platform/lib/js/list.js");
 var $$Array = require("bs-platform/lib/js/array.js");
+var Block = require("bs-platform/lib/js/block.js");
+var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
 var ReasonReact = require("reason-react/src/ReasonReact.js");
 
 var component = ReasonReact.reducerComponent("TodoApp");
+
+function newItem() {
+  return /* record */[
+          /* title */"My new todo",
+          /* completed */false
+        ];
+}
 
 function str(prim) {
   return prim;
@@ -23,13 +32,18 @@ function make() {
           /* willUnmount */component[/* willUnmount */6],
           /* willUpdate */component[/* willUpdate */7],
           /* shouldUpdate */component[/* shouldUpdate */8],
-          /* render */(function (param) {
-              var items = param[/* state */1][/* items */0];
+          /* render */(function (self) {
+              var match = self[/* state */1];
+              var items = match[/* items */0];
               var numberOfTodos = List.length(items);
-              return React.createElement("div", undefined, $$Array.of_list(List.map((function (todo) {
-                                    var match = todo[/* completed */1];
-                                    return React.createElement("div", undefined, React.createElement("div", undefined, todo[/* title */0]), React.createElement("div", undefined, match ? "Done" : "Pending"));
-                                  }), items)), React.createElement("div", undefined, "There are " + (String(numberOfTodos) + " todo(s)")));
+              return React.createElement("div", undefined, "Want to add a todo", React.createElement("button", {
+                              onClick: (function () {
+                                  return Curry._1(self[/* send */3], /* AddItem */0);
+                                })
+                            }, "Add something"), React.createElement("div", undefined, $$Array.of_list(List.map((function (todo) {
+                                        var match = todo[/* completed */1];
+                                        return React.createElement("div", undefined, React.createElement("div", undefined, todo[/* title */0]), React.createElement("div", undefined, match ? "Done" : "Pending"));
+                                      }), items)), React.createElement("div", undefined, "There are " + (String(numberOfTodos) + " todo(s)"))));
             }),
           /* initialState */(function () {
               return /* record */[/* items : :: */[
@@ -41,8 +55,14 @@ function make() {
                       ]];
             }),
           /* retainedProps */component[/* retainedProps */11],
-          /* reducer */(function (_, _$1) {
-              return /* NoUpdate */0;
+          /* reducer */(function (_, param) {
+              return /* Update */Block.__(0, [/* record */[/* items : :: */[
+                            /* record */[
+                              /* title */"My new todo",
+                              /* completed */false
+                            ],
+                            param[/* items */0]
+                          ]]]);
             }),
           /* subscriptions */component[/* subscriptions */13],
           /* jsElementWrapped */component[/* jsElementWrapped */14]
@@ -50,6 +70,7 @@ function make() {
 }
 
 exports.component = component;
+exports.newItem = newItem;
 exports.str = str;
 exports.make = make;
 /* component Not a pure module */
--- src/TodoApp.re
+++ src/TodoApp.re
@@ -5,30 +5,47 @@ type item = {
 
 type state = {items: list(item)};
 
+type action =
+  | AddItem;
+
 let component = ReasonReact.reducerComponent("TodoApp");
 
-let str = ReasonReact.string
+let newItem = () => {title: "My new todo", completed: false};
+
+let str = ReasonReact.string;
 
 let make = _children => {
   ...component,
   initialState: () => {items: [{title: "Add to do", completed: false}]},
-  reducer: ((), _) => ReasonReact.NoUpdate,
-  render: ({state: {items}}) => {
+  reducer: (action, {items}) =>
+    switch (action) {
+    | AddItem => ReasonReact.Update({items: [newItem(), ...items]})
+    },
+  render: (self) => {
+    let { items } = self.state;
     let numberOfTodos = List.length(items);
     <div>
-      (ReasonReact.array(
-        Array.of_list(
-          List.map((todo) =>
-            <div>
-              <div>(str(todo.title))</div>
-              <div>(str(todo.completed ? "Done" : "Pending"))</div>
-            </div>,
-            items
+      (str("Want to add a todo"))
+      <button onClick=((_ => self.send(AddItem)))> (str("Add something")) </button>
+      <div>
+        (
+          ReasonReact.array(
+            Array.of_list(
+              List.map(
+                todo =>
+                  <div>
+                    <div> (str(todo.title)) </div>
+                    <div> (str(todo.completed ? "Done" : "Pending")) </div>
+                  </div>,
+                items,
+              ),
+            ),
           )
         )
-      ))
-
-      <div> (str("There are " ++ string_of_int(numberOfTodos) ++ " todo(s)")) </div>
-    </div>
+        <div>
+          (str("There are " ++ string_of_int(numberOfTodos) ++ " todo(s)"))
+        </div>
+      </div>
+    </div>;
   },
 };
